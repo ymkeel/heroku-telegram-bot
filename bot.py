fe4b3dbbff6676ca93a0b767ee294d7327ca2f7d
ymkeel <kesselyanniel@gmail.com>
2021-12-04 04:20:57 +0000

    Python-Telegram-Bot Example

--- bot.py
+++ bot.py
@@ -2,13 +2,13 @@ from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 import os
 
-BOT_KEY = os.environ['BOT_KEY']
+# BOT_KEY = os.environ['BOT_KEY']
 
 def hello(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
 
-updater = Updater(BOT_KEY)
+updater = Updater('5060494830:AAHYMbYNGz9gTW73yCKRSgxB0UgLPPZlcK4')
 
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
 
